3bce4eb8cb4d5ba74c8ce50210f4e99a5b74f4b4
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-10-25 23:06:35 +0900

    remove: Remove global makeNoteOn() and makeNoteOff()

--- include/pum_Generator.hpp
+++ include/pum_Generator.hpp
@@ -13,44 +13,6 @@
 #include <cassert>
 #include <cstdint>
 
-//==============================================================================
-//Function
-namespace pum
-{
-/** Make USB-MIDI note on packet.
-    @param noteNumber MIDI note number [0, 127]
-    @param velocity MIDI note velocity [0, 127]
-    @param channel MIDI channel [0, 15]
-    @return midiEventPacket_t 
-*/
-inline UsbMidiEventPacket makeNoteOn (uint8_t noteNumber, uint8_t velocity, uint8_t channel)
-{
-    UsbMidiEventPacket p;
-    p.header = CodeIndexNumber::NOTE_ON;
-    p.byte1 = ChannelMessage::NOTE_ON | channel;
-    p.byte2 = noteNumber;
-    p.byte3 = velocity;
-    return p;
-}
-
-/** Make USB-MIDI note off packet.
-    @param noteNumber MIDI note number [0, 127]
-    @param channel MIDI channel [0, 15]
-    @return midiEventPacket_t 
-*/
-inline UsbMidiEventPacket makeNoteOff (uint8_t noteNumber, uint8_t channel)
-{
-    UsbMidiEventPacket p;
-    p.header = CodeIndexNumber::NOTE_OFF;
-    p.byte1 = ChannelMessage::NOTE_OFF | channel;
-    p.byte2 = noteNumber;
-    p.byte3 = 0; ///< velocity 0
-    return p;
-}
-} // namespace pum
-
-//==============================================================================
-//Class
 namespace pum
 {
 /// USB-MIDI packet generator.
