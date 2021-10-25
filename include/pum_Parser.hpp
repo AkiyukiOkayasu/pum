fc9ebc234b009c0ade8b63c0e2196b4779099064
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-10-25 23:07:03 +0900

    feat: Add MIDI parser

--- include/pum_Parser.hpp
+++ include/pum_Parser.hpp
@@ -8,7 +8,12 @@
 
 #pragma once
 
+#include "pum_Constants.hpp"
+#include "pum_Types.hpp"
+
+#include <array>
 #include <cstdint>
+#include <functional>
 
 namespace pum
 {
@@ -18,13 +23,194 @@ public:
     Parser() = default;
     ~Parser() = default;
 
-    /** Parse MIDI packet    
-        @param packet 
-    */
-    void parse (const uint8_t (&packet)[4])
+    void parse (const uint8_t packet[4])
     {
+        const uint8_t codeIndex = codeIndexNumber (packet[0]);
+        const uint8_t cn = cableNumber (packet[0]);
+        const uint8_t statusByte = packet[1];
+        const uint8_t channel = midiCh (statusByte);
+
+        switch (codeIndex)
+        {
+            case CodeIndexNumber::SYSCOMMON_2BYTES:
+                break;
+            case CodeIndexNumber::SYSCOMMON_3BYTES:
+                break;
+            case CodeIndexNumber::SYSEX_START_OR_CONTINUE:
+                if (packet[1] == SysEx::BEGIN)
+                {
+                    //SysEx start
+                    receivingSysEx = true;
+                    sysExIndex = 0;
+                    pushSysEx (packet[2]);
+                    pushSysEx (packet[3]);
+                }
+                else
+                {
+                    //SysEx continue
+                    pushSysEx (packet[1]);
+                    pushSysEx (packet[2]);
+                    pushSysEx (packet[3]);
+                }
+                break;
+            case CodeIndexNumber::SYSEX_END_1BYTE:
+                assert (packet[1] == SysEx::END);
+                receivingSysEx = false;
+                if (onSysEx)
+                {
+                    onSysEx (sysExData.data(), sysExIndex);
+                }
+                break;
+            case CodeIndexNumber::SYSEX_END_2BYTES:
+                assert (packet[2] == SysEx::END);
+                pushSysEx (packet[1]);
+                receivingSysEx = false;
+                if (onSysEx)
+                {
+                    onSysEx (sysExData.data(), sysExIndex);
+                }
+                break;
+            case CodeIndexNumber::SYSEX_END_3BYTES:
+                assert (packet[3] == SysEx::END);
+                pushSysEx (packet[1]);
+                pushSysEx (packet[2]);
+                receivingSysEx = false;
+                if (onSysEx)
+                {
+                    onSysEx (sysExData.data(), sysExIndex);
+                }
+
+                break;
+            case CodeIndexNumber::NOTE_OFF:
+                if (onNoteOff)
+                {
+                    Note note = { packet[2], packet[3], channel };
+                    onNoteOff (note);
+                }
+                break;
+            case CodeIndexNumber::NOTE_ON:
+            {
+                const uint8_t noteNumber = packet[2];
+                const uint8_t velocity = packet[3];
+                Note note = { noteNumber, velocity, channel };
+                if (velocity == 0) // velocity 0 = noteOFF
+                {
+                    if (onNoteOff)
+                    {
+                        onNoteOff (note);
+                    }
+                    break;
+                }
+
+                if (onNoteOn)
+                {
+                    onNoteOn (note);
+                }
+                break;
+            }
+            case CodeIndexNumber::POLYPHONIC_KEY_PRESSURE:
+                break;
+            case CodeIndexNumber::CONTROL_CHANGE:
+                if (onControlChange)
+                {
+                    ControlChange cc = { packet[2], packet[3], channel };
+                    onControlChange (cc);
+                }
+                break;
+            case CodeIndexNumber::PROGRAM_CHANGE:
+                break;
+            case CodeIndexNumber::CHANNEL_PRESSURE:
+                break;
+            case CodeIndexNumber::PITCH_BEND:
+                break;
+            case CodeIndexNumber::SYS_REALTIME:
+                if (onSystemRealtime)
+                {
+                    onSystemRealtime (statusByte);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
+    /** Note on callback.
+      You can assign a lambda to this callback object to have it called when the
+     MIDI note on is come.
+      @see MIDI::Note
+    */
+    std::function<void (Note)> onNoteOn;
+
+    /** Note off callback.
+      You can assign a lambda to this callback object to have it called when the
+     MIDI note off is come.
+      @see MIDI::Note
+    */
+    std::function<void (Note)> onNoteOff;
+
+    /** Control change callback.
+      You can assign a lambda to this callback object to have it called when the
+     MIDI control change is come.
+      @see MIDI::ControlChange
+    */
+    std::function<void (ControlChange)> onControlChange;
+
+    /** SysEx callback.
+      You can assign a lambda to this callback object to have it called when the
+     MIDI SysEx is come.
+      @param SysExByteArray
+      @param SysExByteArraySize
+    */
+    std::function<void (const uint8_t[], const uint8_t)> onSysEx;
+
+    /** SystemRealtime callback.
+      @param byte SystemRealtime is 1byte message.
+      @see SystemRealtime
+    */
+    std::function<void (uint8_t byte)> onSystemRealtime;
+
 private:
+    constexpr uint8_t midiCh (const uint8_t statusByte) const
+    {
+        // MIDI StatusByte下位4bit: MIDI channel
+        return statusByte & 0x0F;
+    }
+
+    /**
+      USB-MIDIイベントパケットのbyte0（midiEventPacket_tのheader)からCable
+     Numberを取得する https://pcm1723.hateblo.jp/entry/20150109/1420819489
+      @param header midiEventPacket_t の header
+      @return uint8_t USB-MIDI Cable Number
+      @note USB-MIDIデバイスが複数使用されているときのインデックスとして利用する
+    */
+    constexpr uint8_t cableNumber (const uint8_t header) const
+    {
+        // Header上位4bit: CableNumber
+        return (header >> 4) & 0x0F;
+    }
+
+    /**
+      USB-MIDIイベントパケットのbyte0（midiEventPacket_tのheader)からCode Index
+     Numberを取得する https://pcm1723.hateblo.jp/entry/20150109/1420819489
+      @param header  midiEventPacket_t の header
+      @return uint8_t USB-MIDI
+      @note USB-MIDIメッセージの種別を表す
+    */
+    constexpr uint8_t codeIndexNumber (const uint8_t header) const
+    {
+        // Header下位4bit: CodeIndexNumber
+        return header & 0x0F;
+    }
+
+    void pushSysEx (uint8_t d)
+    {
+        sysExData[sysExIndex] = d;
+        sysExIndex++;
+    }
+
+    static constexpr uint8_t MAX_SYSEX_LENGTH = 32;
+    bool receivingSysEx = false;
+    uint8_t sysExIndex = 0;
+    std::array<uint8_t, MAX_SYSEX_LENGTH> sysExData;
 };
 } // namespace pum
